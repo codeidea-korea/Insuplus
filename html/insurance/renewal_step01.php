2d9a6e6ab5091be5a9ede2d91909cfdbbe9d86c9
hjlee233 <hjlee233@gmail.com>
2024-06-24 13:42:00 +0900

    hotfix: 단기 최대 귀국일 값 수정

--- html/insurance/renewal_step01.php
+++ html/insurance/renewal_step01.php
@@ -1250,7 +1250,7 @@ include '../_include/_top.html';
     const cHour = cDate.getHours() + 1;
     const dDate = new Date(event.target.value);
     const oneDay = 24 * 60 * 60 * 1000;
-    const ninetyDays = 90 * oneDay;
+    const ninetyDays = 89 * oneDay;
     const oneYear = 364 * oneDay;
     const arrivalElement = document.getElementById('A-arrival');
 
