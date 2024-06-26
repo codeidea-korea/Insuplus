b5210e05c583bf40c2ea0d591f4ba52e65af172b
hjlee233 <hjlee233@gmail.com>
2024-06-26 16:36:22 +0900

    feat: 랜딩페이지 URL 단축 처리

--- move.php
+++ move.php
@@ -1,8 +1,18 @@
 <?
 include_once $_SERVER["DOCUMENT_ROOT"]."/_config/lib.php";
 $moveEvent = REQSTR($_GET["cd"], "");
+$LandingURL = "/html/customer/landing";
 
-if(!empty($moveEvent)){
-  header( 'Location: https://www.insuplus.co.kr/html/main/' );
+if(!empty($moveEvent)) {
+  switch($moveEvent) {
+    case "sinhancard":
+      header( "Location: ".$LandingURL."/sinhancard/sinhancard.php" );
+      break;
+    default:
+    header( "Location: /html/main/" );
+      break;
+  }
+} else {
+  header( "Location: /html/main/" );
 }
 ?>
