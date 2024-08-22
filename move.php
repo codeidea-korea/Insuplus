7644247609ebf8884078af287cfbd1191ad341bc
hjlee233 <hjlee233@gmail.com>
2024-08-22 13:14:08 +0900

    fix: myshop 단축 URL 추가

--- move.php
+++ move.php
@@ -12,7 +12,7 @@ if(!empty($moveEvent)) {
       header( "Location: ".$LandingURL."/daytour/daytour_landing.php" );
       break;
     case "myshop":
-      header( "Location: /html/insurance/partner_step01.php" );
+      header( "Location: /html/insurance/partner_step01.php?alliance_code=TVRiRCtmaWVIZEZBT3Y3QWk0b09Tdz09" );
       break;
     default:
     header( "Location: /html/main/" );
