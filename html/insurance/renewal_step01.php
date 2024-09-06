6ec73412cc2d913645f7a1462648e9fa1877c461
hjlee233 <hjlee233@gmail.com>
2024-09-06 13:50:47 +0900

    feat: I-09-07 / 간편가격조회 step1 텍스트 추가 수정

--- html/insurance/renewal_step01.php
+++ html/insurance/renewal_step01.php
@@ -569,7 +569,7 @@ include '../_include/_top.html';
         html.push(`    </div>`);
         html.push(`  </div>`);
         html.push(`  <div class="title-box mt12">`);
-        html.push(`    <h3>24시간 의료상담 및 병원예약, 의료·여행편의 지원</h3>`);
+        html.push(`    <h3>의료상담 및 병원예약, 여행편의 지원</h3>`);
         html.push(`  </div>`);
         html.push(`  <p class="common-txt01">여행 중 도움이 필요하시면 24시간 언제 어디서든 연락해 주세요.</p>`);
         // html.push(
